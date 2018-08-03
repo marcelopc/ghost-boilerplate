203e515b389b21584ca2881a9324f65aa552f776
Luiz Alexandre <luiz.alexandre@live.com>
2018-08-03 16:42:33 -0300

    config database

--- config/config.js
+++ config/config.js
@@ -1,8 +1,26 @@
 module.exports = {
-    database: { 
-      name: '', 
-      user: '', 
-      password: '' 
+    database: {
+      development: {
+        username: "root",
+        password: "",
+        database: "database_development",
+        host: "127.0.0.1",
+        dialect: "mysql"
+      },
+      test: {
+        username: "root",
+        password: "",
+        database: "database_test",
+        host: "127.0.0.1",
+        dialect: "mysql"
+      },
+      production: {
+        username: "root",
+        password: "null",
+        database: "database_production",
+        host: "127.0.0.1",
+        dialect: "mysql"
+      }
     }, 
     server: { 
       secret: '', 
@@ -11,3 +29,8 @@ module.exports = {
       nome: ''
     }
   } 
+
+
+
+  
+  

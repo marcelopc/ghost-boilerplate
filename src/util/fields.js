4ccfb5da624def69d771bc9a2dd7f342de5514d3
Luiz Alexandre <luiz.alexandre@live.com>
2018-08-07 10:51:10 -0300

    utils

---
+++ src/util/fields.js
@@ -0,0 +1,27 @@
+'use strict';
+
+exports.mapValueFieldNames = function(dataValues, fields, Model) {
+	
+  const values = {};
+
+  for (const attr of fields) {
+    if (dataValues[attr] !== undefined && !Model._isVirtualAttribute(attr)) {
+      // Field name mapping
+      if (Model.rawAttributes[attr] && Model.rawAttributes[attr].field && Model.rawAttributes[attr].field !== attr) {
+        values[Model.rawAttributes[attr].field] = dataValues[attr];
+      } else {
+        values[attr] = dataValues[attr];
+      }
+    }
+  }
+
+  return values;
+
+}
+
+exports.removeFieldUndefineds = function(object) {
+
+	Object.keys(object).forEach(key => object[key] === undefined ? delete object[key] : '');
+	return object
+
+}
---
+++ src/util/file.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+exports.moveFile = function(oldPath, newPath, callback) {
+
+    let dir = newPath.substring(0, newPath.lastIndexOf('/'));
+
+    if (!fs.existsSync(dir)){
+
+        let split = dir.split('/');
+        let diretorio = split.slice(1, split.length);
+        let completDir = '.';
+
+        for (let i = 0; i < diretorio.length; i++) {
+            completDir += '/' + diretorio[i];
+            if (!fs.existsSync(completDir)){
+                fs.mkdirSync(completDir);
+            }
+        }
+    }
+
+
+    fs.rename(oldPath, newPath, function (err) {
+        if (err) {
+
+            if (err.code === 'EXDEV') {
+                copy();
+            } else {
+                if(callback){
+                  callback(err);
+                }
+            }
+            return;
+        }
+        if(callback){
+          callback();
+        }
+    });
+
+    function copy() {
+        var readStream = fs.createReadStream(oldPath);
+
+        var writeStream = fs.createWriteStream(newPath);
+
+        readStream.on('error', callback);
+        writeStream.on('error', callback);
+
+        readStream.on('close', function () {
+            fs.unlink(oldPath, callback);
+        });
+
+        readStream.pipe(writeStream);
+    }
+}
